b477e1709e84c87c2159729084c28818f17ed11b
David <deivid.prz3412@gmail.com>
2025-07-06 14:03:23 -0400

    testtete

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,14 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
+      <h1>Vite + React</h1>
       <h1>Vite + React</h1>
       <h1>Vite + React</h1>
       <h1>Vite + React</h1>
